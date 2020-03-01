527660a1f68a67d31f7acae862457a8edb046ba3
dawsoncanby <dawsoncanby@gmail.com>
2020-03-01 15:50:32 -0700

    added description

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
 let page = 'homepage';
 
-$(document).ready(() => {
-  $('#about').hide();
+function selectPage(newPage) {
+  $('.page').hide()
+  console.log($('.page'));
+  $('#' + newPage).show()
+  console.log(newPage);
+  page = newPage;
+}
 
+
+$(document).ready(() => {
   $('#site-links').children('div').each((i, e) => {
     let link = $(e);
+    let page = link.children('span').html().toLowerCase()
     link.on('click', () => {
-      let newPage = link.children('span').html().toLowerCase();
-      $('#' + page).hide();
-      $('#' + newPage).show();
+      selectPage(page);
     })
   })
+
+  selectPage(page);
 })
