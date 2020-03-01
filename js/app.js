7d03bcf7bdd623508f6292a6501a759b646f152f
dawsoncanby <dawsoncanby@gmail.com>
2020-03-01 14:30:44 -0700

    added logo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
-$(document).ready(() => {
+let page = 'homepage';
 
+$(document).ready(() => {
+  $('#about').hide();
+
+  $('#site-links').children('div').each((i, e) => {
+    let link = $(e);
+    link.on('click', () => {
+      let newPage = link.children('span').html().toLowerCase();
+      $('#' + page).hide();
+      $('#' + newPage).show();
+    })
+  })
 })
